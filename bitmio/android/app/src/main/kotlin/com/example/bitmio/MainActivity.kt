549e4025d0ab01691e0dd1aec3aad9cc6b299696
Mirko Kiefer <mail@mirkokiefer.com>
2020-09-05 17:22:33 +0700

    Add app, configure iOS, Firebase iOS

---
+++ bitmio/android/app/src/main/kotlin/com/example/bitmio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bitmio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bitmio/lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:kollab_template/kollab_template.dart';
+
+void main() => runKollab();
